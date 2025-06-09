1622c82bcba0c0514e7ce0cc2d53bd9913844e27
anas20023 <anasibnebelal400@gmail.com>
2025-06-09 17:13:37 +0600

    refactor: simplify CORS configuration by enabling default settings

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ import {weatherapi} from "./routes/weatherroutes.js"
 dotenv.config();
 const PORT=process.env.PORT||5000;
 const app = express();
-app.use(cors({
-    origin: ['http://localhost:5173/', 'https://www.anasibnbelal.live/', 'https://weather.anasibnbelal.live/']
-}));
+// app.use(cors({
+//     origin: ['http://localhost:5173/', 'https://www.anasibnbelal.live/', 'https://weather.anasibnbelal.live/']
+// }));
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ response: "Hello World" })
